354f1580681ebeff845978445ba985ee1a882179
Den1s11 <145542895+Den1s11@users.noreply.github.com>
2023-09-20 11:36:54 +0300

    Add files via upload

---
+++ OnlineStore/js/script.js
@@ -0,0 +1,34 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const purchaseForm = document.getElementById("purchase-form");
+
+    purchaseForm.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+
+        const nameInput = document.getElementById("name");
+        const userName = nameInput.value;
+
+        
+        alert(`${userName}, благодарим за поръчката!`);
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const reviewForm = document.getElementById("review-form");
+    const reviewContainer = document.querySelector(".user-reviews");
+
+    reviewForm.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+
+        const commentInput = document.getElementById("comment");
+        const userComment = commentInput.value;
+
+        
+        commentInput.value = "";
+
+        
+        const newReview = document.createElement("div");
+        newReview.classList.add("review");
+        newReview.innerHTML = `<p>${userComment}</p>`;
+        reviewContainer.prepend(newReview); 
+    });
+});
+
